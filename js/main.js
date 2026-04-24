4110eb31b7ac081c7c256792ea350c303e1eb29a
Timur <Kudzai1103@gmail.com>
2026-04-24 17:24:28 +0300

    сохраняю перед переключением

---
+++ js/main.js
@@ -0,0 +1,9 @@
+import { photosData } from './mock.js';
+import './thubnails.js';
+// eslint-disable-next-line no-console
+console.log(photosData);
+// eslint-disable-next-line no-console
+// console.log(pictureTemplate);
+// import 'thubnails.js';
+
+
